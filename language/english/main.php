17e4442daeeec2077ab61daa3b4f9ffa244f0a79
Tad <tad0616@gmail.com>
2014-08-05 16:31:43 +0800

    新增 infoway 佈景
    此佈景為馬克開發

---
+++ language/english/main.php
@@ -0,0 +1,65 @@
+<?php
+//選單
+define("THEME_MODULE0", "Main menu");
+define("THEME_MODULE1", "Home");
+
+define("TF_USER_ID",  "Username");
+define("TF_USER_PASS",  "Password");
+define("TF_USER_S_ID",  "Username:");
+define("TF_USER_S_PASS",  "Password:");
+define("TF_USER_ENTER","Login");
+define("TF_USER_EXIT","Logout");
+define("TF_USER_ADMIN","Administration");
+define("TF_USER_BLOCK","Blocks setup");
+define("TF_THEME_ADMIN","Theme setup");
+define("TF_THEME_DEBUG",  "Enable debug");
+define("TF_THEME_UNDEBUG",  "Unable debug");
+define("TF_USER_PROFILE","Profil");
+define("TF_USER_NOTICE","Notifications");
+define("TF_USER_NEWMSG","New message: ");
+define("TF_USER_MSG","Messages");
+define("TF_USER_WELCOME", "Hello ");
+
+define("TF_SEARCH", "Search");
+define("TF_OK", "Submit");
+define("TF_USER_REGIST",  "Create account ?");
+define("TF_USER_FORGET_PASS", "Password lost ?");
+
+define("TF_BLOCK_CONFIG", "Setup this block");
+define("TF_SYSTEM_CONFIG",  "Preferences");
+define("TF_SYSTEM_MODADM",  "Online install");
+define("TF_MODULE_CONFIG",  "Administration");
+define("TF_DL_TAD_THEMES",  "Please install \"Tad Themes\" module.");
+
+
+/**自訂設定之語系**/
+define("TF_MY_MENU", "My Menu");
+define("TF_INFOBAR_BGCOLOR", "Infobar backgroung color");
+define("TF_INFOBAR_BGCOLOR_DESC", "Setup infobar backgroung color");
+
+define("TF_INFOBAR_TEXT", "Infobar text color");
+define("TF_INFOBAR_TEXT_DESC", "Setup infobar text color");
+
+define("TF_FACEBOOK_URL", "facebook url");
+define("TF_FACEBOOK_URL_DESC", "ex:http://www.facebook.com");
+define("TF_TWITTER_URL", "twitter url");
+define("TF_TWITTER_URL_DESC", "ex:http://twitter.com");
+define("TF_GOOGLE_URL", "google+ url");
+define("TF_GOOGLE_URL_DESC", "ex:http://plus.google.com");
+
+
+define("TF_HEADER_IMG", "Header backgroung image");
+define("TF_HEADER_IMG_DESC", "Setup header backgroung image");
+
+define("BT_SEPLINE_COLOR", "Block title underline color");
+define("BT_SEPLINE_COLOR_DESC", "Setup block title underline color");
+
+define("TF_SHOW_VAR", "Display themes vars.");
+define("TF_SHOW_VAR_DESC", "Display all variable of tad_themes module that you could use it to design theme.");
+
+
+
+
+
+
+?>
