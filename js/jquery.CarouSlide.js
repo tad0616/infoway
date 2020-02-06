44309bd376ad7a2fa8e3c81b652fb9ec822135be
Tad <tad0616@gmail.com>
2020-02-06 10:47:41 +0800

    修正logo及滑動圖問題

--- js/jquery.CarouSlide.js
+++ js/jquery.CarouSlide.js
@@ -23,14 +23,14 @@
 			hoverLinks: false, // allows user to change slideshow content on hover, not click
 			easingStyle: null // IF jquery.easing.1.3.js plugin is added to the page, the follow easings are available: swing, easeInQuad, easeOutQuad, easeInOutQuad, easeInCubic, easeOutCubic, easeInOutCubic,easeInQuart, easeOutQuart, easeInOutQuart, easeInQuint, easeOutQuint, easeInOutQuint, easeInSine, easeOutSine, easeInOutSine, easeInExpo, easeOutExpo,easeInOutExpo, easeInCirc, easeOutCirc, easeInOutCirc, easeInElastic, easeOutElastic, easeInOutElastic, easeInBack, easeOutBack, easeInOutBack, easeInBounce, easeOutBounce, easeInOutBounce
 		};
-		
+
 		// constructor function, to hold functions and properties for each CarouSlide implementation
 		function methods(){
 			this.props = {}; // properties object - holds all properties for current instance of CarouSlide
 			var $m = this; // provide access to 'this' for functions (regardless of scope)
-			
+
 			// go to a particular slide	(pos=slide to jump to; direct indicates whether the use
-			// requested a specific page - so we should re-set the timer)	
+			// requested a specific page - so we should re-set the timer)
 			this.gotoSlide = function(pos, direct, dir){
 				pos = parseInt(pos);
 				if (pos != $m.props.currentSlide) { // provided we're not already on the slide we're requesting, continue
@@ -41,7 +41,7 @@
 					$m.props.animQueue = null;
 					$m.props.oldPos = $m.props.currentSlide;
 					$m.props.newPos = null;
-					
+
 					// if we're going directly to a slide (not just back/next)
 					if ($m.props.direct) {
 						if ($m.props.autoAnim &&  $m.props.animState == "play") {
@@ -57,7 +57,7 @@
 					}
 					// highlight the correct active item
 					if ($m.props.showSlideNav) {$m.setNavActive($m.props.currentSlide);}
-					
+
 					// depending on the animation type, change the animation style
 					switch ($m.props.animType) {
 						case "none":
@@ -66,24 +66,24 @@
 							$m.props.direct = null;
 							$m.props.animReady = true;
 							break;
-							
+
 						case "slide":
 							newPos = $m.slideAnimSetup($m.props.sWidth,"left",pos);
 							$m.props.sUL.animate({"left": newPos + "px"}, $m.props.animTime, $m.props.easingStyle, $m.transAnimCallback);
 							break;
-							
+
 						case "slide-vertical":
 							newPos = $m.slideAnimSetup($m.props.sHeight,"top",pos);
 							$m.props.sUL.animate({"top": newPos + "px"}, $m.props.animTime, $m.props.easingStyle, $m.transAnimCallback);
 							break;
-							
+
 						case "fade":
 							var $current = $m.props.sUL.find("> li:eq(" + $m.props.currentSlide + ")");
 							var $prev = $m.props.sUL.find("> li:eq(" + $m.props.prevSlide + ")");
-							
+
 							$current.css("z-index", "100").animate({"opacity":"1"},$m.props.animTime, $m.transAnimCallback);
 							$prev.css("z-index", "10");
-							
+
 							if ($m.props.prevSlide !== null) {
 								setTimeout(function(){
 									$prev.css("opacity","0");
@@ -94,12 +94,12 @@
 					}
 				}
 			};
-			
+
 			// used in both slide and slide-vertical animations - avoiding duplication of effort
 			this.slideAnimSetup = function(dim,dir,pos){
 				var output;
 				var minus = dir == "top" ? 0 : 1;
-				
+
 				if ($m.props.animInfinity) {
 					if ($m.props.direct) {
 						$m.resetAllSlidePositions();
@@ -108,7 +108,7 @@
 					}else {
 						output = -dim + (dim * -$m.props.direction);
 					}
-					
+
 				}else if ($m.props.alwaysNext) {
 					$(".s-id-"+$m.props.oldPos, $m.props.sUL).remove().prependTo($m.props.sUL);
 					$(".s-id-"+$m.props.currentSlide, $m.props.sUL).remove().insertAfter($(".s-id-"+$m.props.oldPos, $m.props.sUL));
@@ -121,7 +121,7 @@
 				}
 				return output;
 			};
-	
+
 			// run when all transition animations complete
 			this.transAnimCallback = function(){
 				if($m.props.animInfinity){
@@ -155,18 +155,18 @@
 					$m.gotoSlide($m.props.animQueue,true,null);
 				}
 			};
-			
+
 			// highlight the correct item in the slideshow nav
 			this.setNavActive = function(id){
 				$("li",$m.props.navContainer).removeClass("active");
 				$("> li:eq(" + id + ")", $m.props.navContainer).addClass("active");
 			};
-			
+
 			// auto increment the slideshow
 			this.timerFunc = function(){
 				$m.gotoSlide($m.props.currentSlide + 1, false, 1);
 			};
-			
+
 			// reset the setInterval timer, that controls the autoAnim
 			this.resetTimer = function(startNow){
 				clearInterval($m.props.timer);
@@ -175,14 +175,14 @@
 					$m.timerFunc();
 				}
 			};
-			
+
 			// put all slides back into their starting order
 			this.resetAllSlidePositions = function(){
 				for(var i=0; i<$m.props.sCount;i++){
 					$(".s-id-"+i, $m.props.sUL).remove().appendTo($m.props.sUL);
 				}
 			};
-			
+
 			// rearrange the slides to the order defined by focusOn (as second slide)
 			this.setSlideOrder = function(focusOn){
 				var firstSlide = (focusOn-1 < 0 ? $m.props.sCount-1 : focusOn-1);
@@ -192,7 +192,7 @@
 					$(".s-id-"+thisSlide, $m.props.sUL).remove().appendTo($m.props.sUL);
 				}
 			};
-			
+
 			// render back/next links on the page
 			this.buildBackNextLinks = function(){
 				var backNextNav = '<ul class="slideshow-back-next">';
@@ -201,13 +201,13 @@
 				backNextNav += '</ul>';
 				$($m.props.sContainer).append(backNextNav);
 			};
-			
+
 			// render pause button on the page
 			this.buildPauseButton = function(){
 				var pauseButton = '<p class="pause-button playing"><a href="javascript:;">Pause</a></p>';
 				$($m.props.sContainer).append(pauseButton);
 			};
-			
+
 			// build navigation for slides
 			this.buildNav = function(){
 				var $nav = $m.props.sUL.after('<ul class="'+$m.props.navContainerTitle+'" />');
@@ -219,16 +219,16 @@
 				}
 				$("."+$m.props.navContainerTitle, $m.props.sContainer).append(slideNavContent);
 			}
-			
+
 			// code to run when initialising the slideshow
 			this.init = function(root){
 				$m.props.sContainer = root;
 				$($m.props.sliderHolder, $m.props.sContainer).wrap("<div class='slider-wrapper' />");
 				$m.props.sWrapper = ($(".slider-wrapper",root));
-				
+
 				// provide styling hook for developers
 				$("> ul",$m.props.sWrapper).addClass("s-active " + $m.props.animType);
-				
+
 				// add values to our $Default variable container, for use in rest of plugin
 				$m.props.timer = null;
 				$m.props.prevSlide = null; // used in animations
@@ -240,22 +240,22 @@
 				$m.props.animState = !$m.props.autoAnim ? "pause" : "play";
 				$m.props.sUL = $(".s-active", $m.props.sWrapper);
 				$m.props.sUL.find("> li",$m.props.sWrapper).each(function(id){$(this).addClass("slide").addClass("s-id-" + id);});
-				$m.props.sCount = $(".s-active > li",$m.props.sWrapper).size();
+				$m.props.sCount = $(".s-active > li",$m.props.sWrapper).length;
 				$m.props.sWidth = $(".s-active > li",$m.props.sWrapper).outerWidth();
 				$m.props.sHeight = $(".s-active > li",$m.props.sWrapper).outerHeight();
 				$m.props.navContainerTitle = $m.props.navContainer.split(".")[1];
-				
+
 				// if we're hovering to activate links, the animation needs to be instantaneous
 				if($m.props.hoverLinks == true){
 					$m.props.animType = "none"
 				}
-				
+
 				// if there is no navigation, and one has been requested, build it now
-				if($m.props.showSlideNav && !$m.props.sContainer.find($m.props.navContainer).size()){
+				if($m.props.showSlideNav && !$m.props.sContainer.find($m.props.navContainer).length){
 					$m.buildNav();
 				}
 				$m.props.navContainer = $m.props.sContainer.find($m.props.navContainer);
-				
+
 				// if there is no animation, force off incompatible features
 				if($m.props.animType == "none"){
 					$m.props.animInfinity = false;
@@ -263,47 +263,47 @@
 				}else{
 					$m.props.hoverLinks = false;
 				}
-				
+
 				// if 'always next', and fading between slides, make it slide (otherwise you won't see any difference)
 				if($m.props.alwaysNext && $m.props.animType == "fade"){
 					$m.props.animType = "slide";
 				}
-				
+
 				// if we're fade between slides, stop the infinity/alwaysNext animation
-				if($m.props.animType == "fade" && ($m.props.animInfinity || $m.props.alwaysNext)){					
+				if($m.props.animType == "fade" && ($m.props.animInfinity || $m.props.alwaysNext)){
 					//Note: animType 'fade' is incompatible with properties 'aninInfinity' and 'alwaysNext'. Both are disabled below.
 					$m.props.animInfinity = false;
 					$m.props.alwaysNext = false;
 				}
-				
+
 				// animInfinity and alwaysNext are incompatible; do one or the other. AlwaysNext takes priority
 				if($m.props.alwaysNext && $m.props.animInfinity){
 					alert("DEVELOPER NOTICE:\n\nProperties 'alwaysNext' and 'aninInfinity' cannot both be active.\n'animInfinity' has been disabled.");
 					$m.props.animInfinity = false;
 				}
-				
+
 				// if we're sliding to infinity, hide the direct slide links
 				if($m.props.animInfinity){
 					$(".slide:last", $m.props.sUL).remove().prependTo($m.props.sUL);
 				}
-				
+
 				// if showing the slideshow main nav, style it for our purposes
 				if($m.props.showSlideNav){
 					$("> li:first", $m.props.navContainer).addClass("active");
 				}else{
 					$($m.props.navContainer).hide();
 				}
-				
+
 				// if we're auto-animating, and the pause button has been requested, render it now
 				if($m.props.autoAnim && $m.props.showPauseButton){
 					$m.buildPauseButton();
 				}
-				
+
 				// if we want back/next buttons, render them now
 				if($m.props.showBackNext){
 					$m.buildBackNextLinks();
 				}
-	
+
 				// depending on the animation type requested, set up the DOM accordingly
 				switch ($m.props.animType) {
 					case "fade":
@@ -327,7 +327,7 @@
 						break;
 					default:
 				}
-				
+
 				// establish auto-animation of slides
 				if ($m.props.autoAnim) {$m.resetTimer();}
 			};
@@ -335,11 +335,11 @@
 
 		// return object for jquery chaining
 		return this.each(function(id, root) {
-			
+
 			var $m = new methods();
 			$m.props = $.extend({}, $.fn.CarouSlide.config, userConfig);
 			$m.init($(this));
-			
+
 			function slideLinkAction(){
 				var pos = $(this).attr("rel");
 				if ($m.props.animReady) {
@@ -363,8 +363,7 @@
 				var ref = s.attr("id");
 				$("a[href=#"+ref+"]").attr({"href": "javascript:;","rel":i}).bind("click", slideLinkAction);
 			};
-			
-			
+
 			// functionality for back/next buttons
 			if ($m.props.showBackNext) {
 				$(".next-button", $m.props.sContainer).click(function(){
@@ -382,12 +381,12 @@
 					}
 				});
 			}
-			
+
 			// pause/play button
 			if($m.props.autoAnim && $m.props.showPauseButton){
 				$($m.props.pauseContainer,$m.props.sContainer).click(function(){
 					if($m.props.animState == "pause"){
-						$m.props.animState = "play";	
+						$m.props.animState = "play";
 						$m.resetTimer(true);
 						$(this).addClass("playing").find("a").text("pause");
 					}
